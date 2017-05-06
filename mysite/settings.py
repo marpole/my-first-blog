3f87b584af047f27544e141bffd971cda3d607cb
marpole <tiino@shaw.ca>
2017-05-06 16:43:43 +0000

    myfirst in pyany

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2!*j66&#2)b!^tip)jdcd+$dj^x)^vo&-3h9qg@3)5sk*!i28i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
